df150519d9e386a14f646fd1468d9c800c0b29f1
Andrews Reis <andrewsdosreis@gmail.com>
2024-10-26 13:37:41 +0200

    Add Maven wrapper and initial project structure
    Introduce Maven wrapper scripts to simplify Maven usage and ensure consistent builds across environments. Add initial project configuration including `.gitignore`, `.gitattributes`, `pom.xml`, and main application class for a Spring Boot application.

---
+++ src/main/java/com/andrewsreis/dundermifflin/DunderMifflinApplication.java
@@ -0,0 +1,13 @@
+package com.andrewsreis.dundermifflin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DunderMifflinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DunderMifflinApplication.class, args);
+    }
+
+}
