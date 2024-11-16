870bd17d59676d40ce0efd91627d9bfca81687c5
Andrews Reis <andrewsdosreis@gmail.com>
2024-11-16 22:38:08 +0100

    Add Docker setup and revise database initialization
    Introduce `docker-compose.yml` to manage `localstack` and `postgres` services. Move database initialization script to `scripts/db-init.sql`, adding a `photo` column and commenting out initial inserts.

--- src/main/resources/db/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE employees (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(100),
-    department VARCHAR(50)
-);
-
-INSERT INTO employees (first_name, last_name, department) VALUES ('Dwight', 'Schrute', 'Sales');
-INSERT INTO employees (first_name, last_name, department) VALUES ('Jim', 'Halpert', 'Sales');
