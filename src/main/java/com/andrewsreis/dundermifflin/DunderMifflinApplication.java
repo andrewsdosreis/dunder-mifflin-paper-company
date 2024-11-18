4a7c4639d39b5e7d24be636cb7a55de272307b36
Andrews Reis <andrewsdosreis@gmail.com>
2024-11-18 22:23:10 +0100

    Enable scheduling in the application
    Added the @EnableScheduling annotation to the main application class. This allows the application to run scheduled tasks using Spring's scheduling capabilities.

--- src/main/java/com/andrewsreis/dundermifflin/DunderMifflinApplication.java
+++ src/main/java/com/andrewsreis/dundermifflin/DunderMifflinApplication.java
@@ -2,8 +2,10 @@ package com.andrewsreis.dundermifflin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DunderMifflinApplication {
 
     public static void main(String[] args) {
